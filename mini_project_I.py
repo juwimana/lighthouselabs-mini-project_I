0630f89df86d2f82f952c236c6fb4533af3c8b0a
Jean Marie Uwimana <juwimana@eagles.usi.edu>
2021-01-07 20:22:42 -0800

    no attribute min error fix

--- mini_project_I.py
+++ mini_project_I.py
@@ -257,13 +257,13 @@ def get_journey_plan(mode_1,mode_2):
 
 		for journey_bus in journey_plan_bus['journeys']:
 			journey_bus_duration.append(int(journey_bus['duration']))
-		bus_duration = journey_bus_duration.min()
+		bus_duration = min(journey_bus_duration)
 
 		journey_plan_tube = res_mode_2.json()
 		journey_tube_duration = []
 		for journey_bus in journey_plan_bus['journeys']:
 			journey_tube_duration.append(int(journey_bus['duration']))
-		tube_duration = journey_tube_duration.min()
+		tube_duration = min(journey_tube_duration)
 
 	return (bus_duration, tube_duration,journey_plan_bus)
 
